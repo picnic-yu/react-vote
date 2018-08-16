7b7887ba7801bd6288f97b17cd2263d850fe77c4
picnic-yu <164005206@qq.com>
2018-08-16 17:49:07 +0800

    手机端图片添加到服务器

--- server/controllers/score.js
+++ server/controllers/score.js
@@ -11,9 +11,7 @@ class ScoreController {
 		 
 		const score = ctx.request.body;
 		
-		console.log(score,'scorescorescorescore')
-		// const userInfo = ctx.cookies.get('userInfo');
-		// console.log(userInfo,'userInfo')
+	
 		if (score.score && score.openid) {
 			const existUser = await scoreModel.findOneByOpenid(score.openid,score.member);
 			console.log(existUser,'existUser')
@@ -22,12 +20,14 @@ class ScoreController {
 					code: 100,
 					message: '该记录已经存在'
 				}
+			}else{
+				await scoreModel.createScore(score)
+				ctx.body = {
+					code: 200,
+					message: '创建成功'
+				}
 			}
-            await scoreModel.createScore(score)
-            ctx.body = {
-                code: 1,
-                message: '创建成功'
-            }
+            
 		} else {
 			ctx.body = {
 				code: -1,
