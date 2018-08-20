727f8510c4b5b9f860eaefbac0b178410f07e11e
陈勃宇 <terry.chen@cloudkeeper.cn>
2018-08-20 10:19:29 +0800

    pc页面成绩公布完成

--- vote-pc/src/pages/vote/component/charts.js
+++ vote-pc/src/pages/vote/component/charts.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { Spin } from 'antd';
 // 引入 ECharts 主模块
 import echarts from 'echarts/lib/echarts';
 // 引入柱状图
@@ -8,11 +8,58 @@ import  'echarts/lib/chart/bar';
 import 'echarts/lib/component/tooltip';
 import 'echarts/lib/component/title';
 import 'echarts/lib/component/legend';
-
+import axios from 'axios';
+import { BASE_URL} from 'src/util';
+const asyncAction = (url) => {
+    return new Promise((resolve,reject) => {
+        axios.get(`${BASE_URL}${url}`).then( (response) =>  {
+            if(response.data.code == 200 ){
+                let data = [];
+                response.data.content.forEach((item)=>{
+                    data.push((item.avg*.25).toFixed(2));
+                });
+                resolve(data);
+            }
+        }).catch( (error) =>  {
+            console.log(error);
+        });
+    })
+}
+const getOtherScoreAction = (url) => {
+    return new Promise((resolve,reject) => {
+        axios.get(`${BASE_URL}${url}`).then( (response) =>  {
+            if(response.data.code == 200 ){
+                resolve(response.data.content);
+            }
+        }).catch( (error) =>  {
+            console.log(error);
+        });
+    })
+}
 class ScoreBar extends Component {
-    componentDidMount() {
+    constructor(props){
+        super(props);
+        this.state = {
+            chartLoading:false
+        }
+    }
+    async componentDidMount() {
+        this.setState({
+            chartLoading:true
+        });
         // 基于准备好的dom，初始化echarts实例
         var myChart = echarts.init(document.getElementById('main'));
+        var expert_score = [68, 75, 66, 75, 80, 85];                           //专家
+        var computer_score = [90, 85, 88, 75, 90, 82];//设备
+        const averageScore = await asyncAction('/score/getAverage') ||  [70, 80, 60, 90, 88, 90,]; //平均分
+        const otherScore = await getOtherScoreAction('/otherscore/getAverage');
+        if(otherScore){
+            computer_score = otherScore.computer_scoreArr;
+            expert_score = otherScore.expert_scoreArr;
+        }
+        this.setState({
+            chartLoading:false
+        });
         // 绘制图表
         let option = {
             tooltip : {
@@ -64,7 +111,7 @@ class ScoreBar extends Component {
                             position: 'inside'
                         }
                     },
-                    data: [70, 80, 60, 90, 88, 90,]
+                    data: averageScore
                 },
                 {
                     name: '设备评分',
@@ -76,7 +123,7 @@ class ScoreBar extends Component {
                             position: 'inside'
                         }
                     },
-                    data: [90, 85, 88, 75, 90, 82]
+                    data: computer_score
                 },
                 {
                     name: '专家评分',
@@ -88,7 +135,7 @@ class ScoreBar extends Component {
                             position: 'inside'
                         }
                     },
-                    data: [68, 75, 66, 75, 80, 85]
+                    data: expert_score
                 }
             ]
         };
@@ -98,10 +145,12 @@ class ScoreBar extends Component {
     render() {
         return (
             <section>
-                <div style={{height: '50px', width: '100%', fontSize:'22px',display: 'flex', alignItems: 'center', justifyContent: 'center',} }>
-                    艾知星设备评分占25%,现场评分平均分占25%,专家评分占50%
-                </div>
-                <div id="main" style={{ width: '600px', height: '600px',margin:'0 auto' }}></div>
+                <Spin spinning={this.state.chartLoading}>
+                    <div style={{height: '50px', width: '100%', fontSize:'22px',display: 'flex', alignItems: 'center', justifyContent: 'center',} }>
+                        艾知星设备评分占25%,现场评分平均分占25%,专家评分占50%
+                    </div>
+                    <div id="main" style={{ width: '600px', height: '600px',margin:'0 auto' }}></div>
+                </Spin>
             </section>
         );
     }
