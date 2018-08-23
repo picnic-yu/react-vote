5e1fabf9d8a0a751bcddab7f5071ecb7886cc62d
陈勃宇 <terry.chen@cloudkeeper.cn>
2018-08-23 17:58:50 +0800

    一步一步样式调整

--- vote-pc/src/pages/vote/component/step.js
+++ vote-pc/src/pages/vote/component/step.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './step.css'
+import { Row, Col } from 'antd';
 export default class Step extends React.Component {
     render(){
         return (
             <div className='step'>
-                <div className='stepWrap'>
-                    <img style={{height: '400px', width: '100%'} } src='http://new.szdtb.com/image/step-logo.png' />
+                <div className='step_wrap'>
+                    <div  ><img  style={{margin:'0 auto',display:'block'} }src='http://new.szdtb.com/image/step-logo.png' /></div>
+                    
+                </div>
+            <div className='step_wrap'>
+            
+            </div>
+                <div className='step_wrap'>
+                    
                     
                     <p className='step-title'>中华口腔医学会</p>
                     <p className='step-title'>第20次全国口腔医学学术会议</p>
@@ -18,7 +26,7 @@ export default class Step extends React.Component {
                     <p className='desStep'>专家现场指导，掌握最实用的技巧和要点，体验最前沿的产品，还能获得国家级I类继续医学教育学分8分。</p>
                     <div>
                         <div className='left'>
-                            <img style={{ width: '100%',height: '500px'} } src='http://new.szdtb.com/image/tan.png' />
+                            <img style={{ } } src='http://new.szdtb.com/image/tan.png' />
                         </div>
                         <div className='right'>
                             <p>谭建国教授</p>
@@ -30,19 +38,30 @@ export default class Step extends React.Component {
                         </div>
                         
                     </div>
+                    <div >
+                        <Row gutter={16} >
+                            <Col  span={12}>
+                                <p className='step-title'><span className=' redColor'>（全瓷冠）</span></p>
+                                <p className='step-title'>时间：8月29日下午、8月30日</p>
+                                <p className='step-title'>全天 9:30-12:00 14:00-16:30</p>
+                                <p className='step-title'>12人/场（1医1护，两人一组）</p>
+                                <p className='step-title'>每半天1场，共3场</p>
+                                <p className='step-title'>内容相同会员免费操作</p>
+                            </Col>
+                            <Col  span={12}>
+                                <p className='step-title'><span className=' redColor'>（全瓷贴面）</span></p>
+                                <p className='step-title'>时间：8月31日全天 </p>
+                                <p className='step-title'>9:30-12:00 14:00-16:30</p>
+                                <p className='step-title'>12人/场（1医1护，两人一组）</p>
+                                <p className='step-title'>每半天1场，共2场内容相同</p>
+                                <p className='step-title'>会员免费操作</p>
+                            </Col>
+                            
+                        
+                        </Row>
+                    </div>
+                   
                     
-                    <p className='step-title'><span className=' redColor'>（全瓷冠）</span></p>
-                    <p className='step-title'>时间：8月29日下午、8月30日</p>
-                    <p className='step-title'>全天 9:30-12:00 14:00-16:30</p>
-                    <p className='step-title'>12人/场（1医1护，两人一组）</p>
-                    <p className='step-title'>每半天1场，共3场</p>
-                    <p className='step-title'>内容相同会员免费操作</p>
-                    <p className='step-title'><span className=' redColor'>（全瓷贴面）</span></p>
-                    <p className='step-title'>时间：8月31日全天 </p>
-                    <p className='step-title'>9:30-12:00 14:00-16:30</p>
-                    <p className='step-title'>12人/场（1医1护，两人一组）</p>
-                    <p className='step-title'>每半天1场，共2场内容相同</p>
-                    <p className='step-title'>会员免费操作</p>
                     <p className='step-title'>地点：国家会展中心（上海）会场20（5.2馆•操作演示）</p>
                     <p className='step-title'>
                         <a href='https://mp.weixin.qq.com/s?__biz=MjM5NTYzNTgwMA==&mid=2651929463&idx=2&sn=b4255ca3b1982ea099ce932667315c32&chksm=bd10ec408a6765563290b44a2d131e1df15affc0d67822bde2f3c8dee272403d484d76c5b446&mpshare=1&scene=1&srcid=0814pwbAbi8vffKZEEbvomZs&key=62369a1ccc4907a98c5ff71490a01580d3ebe99510091fe59721108e1a6f770fbbc3eeb907905e397d863e8f295c5651deab53b0cd7533ebf1f15f0c419186c7846f2ddaf1e806bd3cd838e37c606526&ascene=1&uin=MTExMjY2MDc2MQ%3D%3D&devicetype=Windows+10&version=61020020&lang=zh_CN&pass_ticket=LOCs2x5ZRm6x7r2Aj6ykX66ZPHR82fKTxgrctbdvAm9VBznJhE1mWXLHsh%2FpuoFx'>更多详情请点击（超链接）</a>
